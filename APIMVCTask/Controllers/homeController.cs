b2fc414f6a6a91bca2e4ac269390ad0d40a249ea
samuelsajasi <62615023+samuelsajasi@users.noreply.github.com>
2020-03-24 13:35:48 -0400

    API MVC Task

---
+++ APIMVCTask/Controllers/homeController.cs
@@ -0,0 +1,51 @@
+﻿using APIMVCTask.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace APIMVCTask.Controllers
+{
+    public class homeController : Controller
+    {
+        //Hosted web API Service base url  
+        string Baseurl = "https://rules.sos.ri.gov";
+
+        public async Task<ActionResult> Index()
+        {
+            List<Regulation> regulationInfo = new List<Regulation>();
+
+            using (var client = new HttpClient())
+            {
+                //Passing service base url  
+                client.BaseAddress = new Uri(Baseurl);
+
+                client.DefaultRequestHeaders.Clear();
+
+                //Define request data format  
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                //Sending request to find web api REST service resource api_get_rules_by_org_id_and_keyword using HttpClient  
+                HttpResponseMessage Res = await client.GetAsync("/regulations/api_get_rules_by_org_id_and_keyword/active/108");
+
+                //Checking the response is successful or not which is sent using HttpClient  
+                if (Res.IsSuccessStatusCode)
+                {
+                    //Storing the response details recieved from web api   
+                    var regulationResponse = Res.Content.ReadAsStringAsync().Result;
+
+                    //Deserializing the response recieved from web api and storing into the  list  
+                    regulationInfo = JsonConvert.DeserializeObject<List<Regulation>>(regulationResponse);
+
+                }
+                //returning the employee list to view  
+                return View(regulationInfo);
+            }
+        }
+    }
+}
---
+++ APIMVCTask/Models/Regulations.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace APIMVCTask.Models
+{
+    public class Regulation
+    {
+
+        public string typeOfRule { get; set; }
+        public int OrganizationId { get; set; }
+        public string myKeyword { get; set; }
+
+    }
+}
